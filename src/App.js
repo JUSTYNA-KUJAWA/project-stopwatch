6d03121fffae8c729204b4076c3e5bd5e1a33e36
Justyna KUJAWA <justy.kujawa@gmail.com>
2022-02-01 23:34:10 +0100

    stopwatch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import Stopwatch from "./components/Stopwatch/Stopwatch";
+import Button from "./components/Button/Button";
+import React, {useState} from "react";
+import Container from "./components/Container/Container";
 
 function App() {
+  
+  const [time, setTime] = useState({h: 0, m: 0, s:0, ms:.0});
+  const [interv, setInterv] = useState();
+
+  const start = () => {
+    run();
+    setInterv(setInterval(run, 10));
+  }
+
+  let timerH = time.h, timerM = time.m, timerS= time.s, timerMs = time.ms;
+
+  const run = () => {
+    if(timerM === 60){
+      timerH ++;
+      timerM = 0;
+    }
+    if(timerS === 60){
+      timerM ++;
+      timerS =0
+    }
+    if(timerMs === 100){
+      timerS ++;
+      timerMs = 0
+    }
+    timerMs++;
+    return setTime({h: timerH, m: timerM, s: timerS, ms: timerMs})
+  }
+
+  const pause = () => {
+    clearInterval(interv);
+  }
+
+  const reset = () =>{
+    clearInterval(interv)
+    return setTime({h: 0, m: 0, s: 0, ms: 0})
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Container>
+
+     <Stopwatch time={time}/>
+     <Button start={start} pause={pause}  reset={reset}/>
+
+    </Container>
+  )
 }
 
 export default App;
